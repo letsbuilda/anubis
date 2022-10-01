adab839d59d53ebe19e4d50d46ead4a3fd322e76
Bradley Reynolds <bradley.reynolds@darbia.dev>
2022-09-30 21:47:16 -0500

    start over using botcore template
    Signed-off-by: Bradley Reynolds <bradley.reynolds@darbia.dev>

--- src/bot/__init__.py
+++ src/bot/__init__.py
@@ -1,10 +1,24 @@
-from typing import TYPE_CHECKING
+import asyncio
+import logging
+import os
+import sys
 
-from bot import log
+import botcore
 
-if TYPE_CHECKING:
-    from bot.bot import Bot
+if os.name == "nt":
+    # Change the event loop policy on Windows to avoid exceptions on exit
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-log.setup()
+# Some basic logging to get existing loggers to show
+logging.getLogger().addHandler(logging.StreamHandler())
+logging.getLogger().setLevel(logging.DEBUG)
+logging.getLogger("discord").setLevel(logging.ERROR)
 
-instance: "Bot" = None  # Global Bot instance.
+
+class Bot(botcore.BotBase):
+    """Sample Bot implementation."""
+
+    async def setup_hook(self) -> None:
+        """Load extensions on startup."""
+        await super().setup_hook()
+        asyncio.create_task(self.load_extensions(sys.modules[__name__]))
--- src/bot/__main__.py
+++ src/bot/__main__.py
@@ -1,33 +1,38 @@
 import asyncio
 from os import getenv
+
 import aiohttp
 import discord
-from discord.ext.commands import when_mentioned_or
+import dotenv
+from discord.ext import commands
 
-import bot
-from bot.bot import Bot
-from bot.log import get_logger
+import botcore
+from . import Bot
+
+dotenv.load_dotenv()
+botcore.utils.apply_monkey_patches()
+
+roles = getenv("ALLOWED_ROLES")
+roles = [int(role) for role in roles.split(",")] if roles else []
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = Bot(
+    guild_id=int(getenv("GUILD_ID")),
+    http_session=None,  # type: ignore # We need to instantiate the session in an async context
+    allowed_roles=roles,
+    command_prefix=commands.when_mentioned_or(getenv("PREFIX", "!")),
+    intents=intents,
+    description="Bot-core test bot.",
+)
 
 
 async def main() -> None:
-    """Entry async method for starting the bot."""
-    intents = discord.Intents.default()
-    intents.message_content = True
+    """Run the bot."""
+    bot.http_session = aiohttp.ClientSession()
+    async with bot:
+        await bot.start(getenv("BOT_TOKEN"))
 
-    async with aiohttp.ClientSession() as session:
-        bot.instance = Bot(
-            command_prefix=when_mentioned_or("!"),
-            intents=intents,
-            http_session=session,
-        )
-        async with bot.instance as _bot:
-            await _bot.start(getenv("ANUBIS_TOKEN"))
 
-if __name__ == "__main__":
-    try:
-        asyncio.run(main())
-    except Exception as exception:
-        log = get_logger("bot")
-        log.exception(exception)
-
-        exit(69)
+asyncio.run(main())

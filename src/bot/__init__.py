1c332326cae01de39ffd417026189371fe179c8c
Bradley Reynolds <bradley.reynolds@darbia.dev>
2022-10-01 00:19:48 -0500

    remove Windows monkeypatch
    Signed-off-by: Bradley Reynolds <bradley.reynolds@darbia.dev>

--- src/bot/__init__.py
+++ src/bot/__init__.py
@@ -1,10 +1,4 @@
-import asyncio
 import logging
-import os
-
-if os.name == "nt":
-    # Change the event loop policy on Windows to avoid exceptions on exit
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 # Some basic logging to get existing loggers to show
 logging.getLogger().addHandler(logging.StreamHandler())

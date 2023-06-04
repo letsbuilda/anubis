79047f0555abd704acb9536e5797d86f6864ad77
IlluminatiFish <45714340+IlluminatiFish@users.noreply.github.com>
2023-06-04 21:56:21 +0100

    Update constants.py
    Update constants 
    
    Signed-off-by: IlluminatiFish <45714340+IlluminatiFish@users.noreply.github.com>

--- src/bot/constants.py
+++ src/bot/constants.py
@@ -66,6 +66,8 @@ class _Channels(EnvConfig):
     dev_log = 1012202489342345246
     mod_log = 1087901347040465006
     soc_log = 1087901419132170260
+    
+    soc_category = 1086886764255395900
 
 
 Channels = _Channels()
@@ -76,8 +78,9 @@ class _Roles(EnvConfig):
     """Channel constants"""
 
     moderators = 1087224451571142716
-
-
+    security = 1086881843636359188
+    
+    
 Roles = _Roles()
 
 

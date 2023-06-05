1de782639fcb4a2efbc4cc4259da97fd5b04f47c
Bradley Reynolds <bradley.reynolds@darbia.dev>
2023-06-05 02:45:28 +0000

    init snekbox cog
    Signed-off-by: Bradley Reynolds <bradley.reynolds@darbia.dev>

---
+++ src/bot/exts/utilities/snekbox/__init__.py
@@ -0,0 +1,14 @@
+from bot.bot import Bot
+
+from ._cog import CodeblockConverter, Snekbox
+from ._eval import EvalJob, EvalResult
+
+__all__ = ("CodeblockConverter", "Snekbox", "EvalJob", "EvalResult")
+
+
+async def setup(bot: Bot) -> None:
+    """Load the Snekbox cog."""
+    # Defer import to reduce side effects from importing the codeblock package.
+    from ._cog import Snekbox
+
+    await bot.add_cog(Snekbox(bot))

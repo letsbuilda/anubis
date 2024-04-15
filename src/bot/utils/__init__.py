b69d11b8d8239f8f2d0257924f1614ed8587227f
Bradley Reynolds <bradley.reynolds@darbia.dev>
2024-04-15 20:26:51 +0000

    Remove internal pastebin utils
    Signed-off-by: GitHub <noreply@github.com>

--- src/bot/utils/__init__.py
+++ src/bot/utils/__init__.py
@@ -1,18 +1,11 @@
 """Internal utilites."""
 
 from bot.utils.helpers import CogABCMeta, find_nth_occurrence, has_lines, pad_base64
-from bot.utils.services import (
-    PasteTooLongError,
-    PasteUploadError,
-    send_to_paste_service,
-)
+
 
 __all__ = [
     "CogABCMeta",
-    "PasteTooLongError",
-    "PasteUploadError",
     "find_nth_occurrence",
     "has_lines",
     "pad_base64",
-    "send_to_paste_service",
 ]
